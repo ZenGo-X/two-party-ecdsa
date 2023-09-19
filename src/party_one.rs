6dda2612549a133f2de4cb71a9c7b74528cde25c
Iraklis Leontiadis <iraklis@zengo.com>
2023-09-19 16:51:54 +0300

    remove ser/de

--- src/party_one.rs
+++ src/party_one.rs
@@ -51,7 +51,7 @@ use crate::curv::GE;
 use crate::Error::{self, InvalidSig};
 
 #[typetag::serde(tag = "type")]
-pub trait Value: Sync + Send + Serialize + Deserialize {}
+pub trait Value: Sync + Send   {}
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
 pub struct HDPos {
