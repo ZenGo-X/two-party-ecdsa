ac61c09302d9ed89da68ff21c0f74552f23a41d3
Iraklis Leontiadis <iraklis@zengo.com>
2023-09-22 14:53:10 +0300

    chaincode

--- src/kms/ecdsa/two_party/mod.rs
+++ src/kms/ecdsa/two_party/mod.rs
@@ -1,9 +1,13 @@
+use std::any::Any;
+use std::fmt::{Display, Formatter};
 use serde::{Deserialize, Serialize};
 use crate::curv::arithmetic::traits::Converter;
 use crate::curv::cryptographic_primitives::hashing::{hmac_sha512, traits::KeyedHash};
 use crate::curv::elliptic::curves::traits::{ECPoint, ECScalar};
 use crate::curv::{BigInt, FE, GE};
 use crate::{paillier::EncryptionKey, party_one, party_two};
+use crate::kms::chain_code::two_party::party1::ChainCode1;
+use crate::party_one::Value;
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
 pub struct Party1Public {
@@ -14,12 +18,24 @@ pub struct Party1Public {
     pub c_key: BigInt,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Clone,Debug,Serialize, Deserialize)]
 pub struct MasterKey1 {
     pub public: Party1Public,
     // Why is the field below public? See: https://github.com/KZen-networks/kms-secp256k1/issues/20
     pub private: party_one::Party1Private,
     chain_code: BigInt,
+}
+#[typetag::serde]
+impl Value for MasterKey1 {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
+impl Display for MasterKey1 {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
